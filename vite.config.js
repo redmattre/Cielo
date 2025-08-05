c9e72637b36944138d6d2ddd05d4234b5d06a48f
redmattre <65075982+redmattre@users.noreply.github.com>
2025-08-05 14:43:54 +0200

    Build change #2

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    // base: "/Cielo/", // YOUR REPO NAME HERE
-    
+    base: "/Cielo/", // YOUR REPO NAME HERE
 });
+
+/*
+l'unico modo che ho trovato per ora è fare due build cambiando manualmente
+questo file. 
+e lo chiami "/Cielo/" funziona su GitHub Pages
+Se non lo chiami e lasci vuoto funziona in locale.
+*/ 
